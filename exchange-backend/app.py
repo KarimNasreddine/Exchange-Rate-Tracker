74eca1d8d3c4b070b274c4f74fed1f1e5c85770a
yga03 <73024078+yga03@users.noreply.github.com>
2022-04-26 00:05:38 +0300

    Added /insights endpoint to get average, open, close, volume statistics. Return statement left empty. Database URL needs to be changed.

--- exchange-backend/app.py
+++ exchange-backend/app.py
@@ -126,6 +126,122 @@ def authenticate():
         }
     )
 
+@app.route('/graph', methods=['GET'])
+def get_daily_rate():
+    transactions = Transaction.query.all()
+
+    usd_to_lbp_dict = {}
+    lbp_to_usd_dict = {}
+
+    # today's date
+    today = datetime.datetime.now()
+    # adding today and last 10 days to each dictionary
+    for i in range(10):
+        usd_to_lbp_dict[today.strftime("%Y-%m-%d")] = 0
+        lbp_to_usd_dict[today.strftime("%Y-%m-%d")] = 0
+        today = today - datetime.timedelta(days=1)
+
+    # function to get the rate of usd_to_lbp transactions of certain day # def get_usd_to_lbp_rate(date):
+
+    # function to get the rate of lbp_to_usd transactions of certain day # def get_lbp_to_usd_rate(date):
+
+    # loop over each key of usd_to_lbp_dict and get the rate of usd_to_lbp transactions of that day
+    for key in usd_to_lbp_dict:
+        usd_to_lbp_dict[key] = get_usd_to_lbp_rate(key, transactions)
+
+    # loop over each key of lbp_to_usd_dict and get the rate of lbp_to_usd transactions of that day
+    for key in lbp_to_usd_dict:
+        lbp_to_usd_dict[key] = get_lbp_to_usd_rate(key, transactions)
+
+    return jsonify({'sell': usd_to_lbp_dict, 'buy': lbp_to_usd_dict})
+
+@app.route('/insights', methods=['GET'])  # average, open, close, volume, biggest transaction of past numb of days
+def get_insights():
+    usd_to_lbp_avg = {}  # average sell
+    lbp_to_usd_avg = {}  # average buy
+    numb = 10
+
+    usd_to_lbp_open = {}  # open sell
+    lbp_to_usd_open = {}  # open buy
+    usd_to_lbp_clos = {}  # close sell
+    lbp_to_usd_clos = {}  # close buy
+    volume_in_trxs = {}  # number of transactions per day in last 2 weeks
+    volume_in_usd = {}  # amount of USD transacted with per day in last 2 weeks
+
+    today = datetime.datetime.now()
+
+    transactions = Transaction.query.filter(
+        Transaction.added_date.between(datetime.datetime.now() - datetime.timedelta(days=numb),
+                                       datetime.datetime.now())).all()
+    buy_transactions = Transaction.query.filter(
+        Transaction.added_date.between(datetime.datetime.now() - datetime.timedelta(days=numb),datetime.datetime.now()),
+        Transaction.usd_to_lbp == 1).all()
+
+    sell_transactions = Transaction.query.filter(
+        Transaction.added_date.between(datetime.datetime.now() - datetime.timedelta(days=numb),datetime.datetime.now()),
+        Transaction.usd_to_lbp == 0).all()
+
+    for trx in range(len(transactions)):
+        usd_to_lbp_avg[today.strftime("%Y-%m-%d")] = 0
+        lbp_to_usd_avg[today.strftime("%Y-%m-%d")] = 0
+
+        # usd_to_lbp_open[today.strftime("%Y-%m-%d")] = 0
+        # lbp_to_usd_open[today.strftime("%Y-%m-%d")] = 0
+
+        today = today - datetime.timedelta(days=1)
+
+    for trx in range(len(buy_transactions)):
+        if trx == 1:
+            usd_to_lbp_open[buy_transactions[trx-1].added_date.strftime("%Y-%m-%d")] = buy_transactions[trx-1].lbp_amount / buy_transactions[trx-1].usd_amount
+        if buy_transactions[trx].added_date.strftime("%Y-%m-%d") != buy_transactions[trx - 1].added_date.strftime("%Y-%m-%d"):
+            usd_to_lbp_open[buy_transactions[trx].added_date.strftime("%Y-%m-%d")] = buy_transactions[trx].lbp_amount / buy_transactions[trx].usd_amount
+
+        if trx == len(buy_transactions) - 1:
+            usd_to_lbp_clos[buy_transactions[trx].added_date.strftime("%Y-%m-%d")] = buy_transactions[trx].lbp_amount / buy_transactions[trx].usd_amount
+            break
+        if buy_transactions[trx].added_date.strftime("%Y-%m-%d") != buy_transactions[trx + 1].added_date.strftime("%Y-%m-%d"):
+            usd_to_lbp_clos[buy_transactions[trx].added_date.strftime("%Y-%m-%d")] = buy_transactions[trx].lbp_amount / buy_transactions[trx].usd_amount
+
+    for trx in range(len(sell_transactions)):
+        if trx == 1:
+            lbp_to_usd_open[sell_transactions[trx-1].added_date.strftime("%Y-%m-%d")] = sell_transactions[trx-1].lbp_amount / sell_transactions[trx-1].usd_amount
+        if sell_transactions[trx].added_date.strftime("%Y-%m-%d") != sell_transactions[trx - 1].added_date.strftime("%Y-%m-%d"):
+            lbp_to_usd_open[sell_transactions[trx].added_date.strftime("%Y-%m-%d")] = sell_transactions[trx].lbp_amount / sell_transactions[trx].usd_amount
+
+        if trx == len(sell_transactions) - 1:
+            lbp_to_usd_clos[sell_transactions[trx].added_date.strftime("%Y-%m-%d")] = sell_transactions[trx].lbp_amount / sell_transactions[trx].usd_amount
+            break
+        if sell_transactions[trx].added_date.strftime("%Y-%m-%d") != sell_transactions[trx + 1].added_date.strftime("%Y-%m-%d"):
+            lbp_to_usd_clos[sell_transactions[trx].added_date.strftime("%Y-%m-%d")] = sell_transactions[trx].lbp_amount / sell_transactions[trx].usd_amount
+
+    # loop over each key of usd_to_lbp_dict and get the rate of usd_to_lbp transactions of that day
+    for key in usd_to_lbp_avg:
+        usd_to_lbp_avg[key] = get_usd_to_lbp_rate(key, transactions)
+    for key in lbp_to_usd_avg:
+        lbp_to_usd_avg[key] = get_lbp_to_usd_rate(key, transactions)
+
+    today = datetime.datetime.now()
+
+    for i in range(14):  # date in form of "2022-04-24"
+        date_to_check = (datetime.datetime.now() - datetime.timedelta(days=i)).strftime("%Y-%m-%d")
+        trx_on_day = Transaction.query.filter(Transaction.added_date.between(today - datetime.timedelta(days=1), today)).all()
+
+        volume_in_trxs[date_to_check] = len(trx_on_day)
+
+        usd_sum = 0
+        for j in trx_on_day:
+            usd_sum += j.usd_amount
+
+        volume_in_usd[date_to_check] = usd_sum
+
+        # print(trx_on_day, file=sys.stderr)
+        today = today - datetime.timedelta(days=1)
+
+    return ""  # whatever you need
+
+
+db.create_all()
+
 
 def find_average(transactions):
     average = 0
@@ -162,3 +278,21 @@ def extract_auth_token(authenticated_request):
 def decode_token(token):
     payload = jwt.decode(token, SECRET_KEY, 'HS256')
     return payload['sub']
+
+def get_usd_to_lbp_rate(date, transactions):
+    daily_rate = []
+    for i in transactions:
+        if i.usd_to_lbp == True and i.added_date.strftime("%Y-%m-%d") == date:
+            daily_rate.append(i.lbp_amount / i.usd_amount)
+    if len(daily_rate) == 0:
+        return 0
+    return sum(daily_rate) / len(daily_rate)
+
+def get_lbp_to_usd_rate(date, transactions):
+    daily_rate = []
+    for i in transactions:
+        if i.usd_to_lbp == False and i.added_date.strftime("%Y-%m-%d") == date:
+            daily_rate.append(i.lbp_amount / i.usd_amount)
+    if len(daily_rate) == 0:
+        return 0
+    return sum(daily_rate) / len(daily_rate)
