73ca121bb243b8e600a4f1231e508ac651ddffe1
Maarouf Yassine <maarouf_yassine@hotmail.com>
2022-05-07 19:12:57 +0300

    Fix DB Initialization Issue - Backend

--- exchange-backend/app.py
+++ exchange-backend/app.py
@@ -13,7 +13,7 @@ from flask_bcrypt import Bcrypt
 
 # Import the database configurations by creating a db_config.py file and
 # adding the configurations in DB_CONFIG variable
-# from .db_config import DB_CONFIG
+from db_config import DB_CONFIG
 
 app = Flask(__name__)
 
@@ -21,72 +21,13 @@ ma = Marshmallow(app)
 
 bcrypt = Bcrypt(app)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONFIG
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:0000@localhost:3306/exchange"
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONFIG
 CORS(app)
 db = SQLAlchemy(app)
 
-# from .model.user import User, UserSchema
-# from .model.transaction import Transaction, TransactionSchema
-# from .model.listing import Listing, ListingSchema
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(30), unique=True)
-    hashed_password = db.Column(db.String(128))
-
-    def __init__(self, user_name, password):
-        super(User, self).__init__(user_name=user_name)
-        self.hashed_password = bcrypt.generate_password_hash(password)
-
-
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ("id", "user_name")
-        model = User
-
-
-class Transaction(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    usd_amount = db.Column(db.Float)
-    lbp_amount = db.Column(db.Float)
-    usd_to_lbp = db.Column(db.Boolean)
-    added_date = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
-
-    def __init__(self, usd_amount, lbp_amount, usd_to_lbp, user_id):
-        super(Transaction, self).__init__(usd_amount=usd_amount,
-                                          lbp_amount=lbp_amount, usd_to_lbp=usd_to_lbp,
-                                          user_id=user_id,
-                                          added_date=datetime.datetime.now())
-
-
-class TransactionSchema(ma.Schema):
-    class Meta:
-        fields = ("id", "usd_amount", "lbp_amount", "usd_to_lbp", "added_date", "user_id")
-        model = Transaction
-
-
-
-
-
-class Listing(db.Model):
-    ListingID = db.Column(db.Integer, primary_key=True)
-    PostingUserID = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    UserPhoneNumer = db.Column(db.String(20), nullable=False)
-    MoneyAmount = db.Column(db.Float, nullable=False)
-    Usd_To_Lbp = db.Column(db.Boolean, nullable=False)
-    Resolved = db.Column(db.Boolean, nullable=True)
-    ResolvedByUser = db.Column(db.Integer, nullable=True)
-
-    # def __init__(self, PostingUserID, UserPhoneNumer, MoneyAmount, Usd_To_Lbp, Resolved, ResolvedByUser):
-    #     super(Listing, self).__init__()
-
-class ListingSchema(ma.Schema):
-    class Meta:
-        fields = ('ListingID', 'PostingUserID', 'UserPhoneNumer', 'MoneyAmount', 'Usd_To_Lbp', 'Resolved', 'ResolvedByUser')
-        model = Listing
+from model.user import User, UserSchema
+from model.transaction import Transaction, TransactionSchema
+from model.listing import Listing, ListingSchema
 
 
 SECRET_KEY = "b'|\xe7\xbfU3`\xc4\xec\xa7\xa9zf:}\xb5\xc7\xb9\x139^3@Dv'"
@@ -124,8 +65,6 @@ def get_listings():
     #get all the listings that are not resolved 
     listings = Listing.query.filter_by(Resolved=False).all()
     return jsonify(listing_schema.dump(listings, many=True))
-
-
     
 
 @app.route('/transaction', methods=['POST'])
