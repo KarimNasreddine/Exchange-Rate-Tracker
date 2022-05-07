b7656c21b92e1338bfeaef2372d96523af0b046c
Firas-AM <81926356+Firas-AM@users.noreply.github.com>
2022-05-07 18:49:52 +0300

    added backend for listings

---
+++ exchange-backend/model/listing.py
@@ -0,0 +1,19 @@
+# from ..app import db, ma
+
+# class Listing(db.Model):
+#     ListingID = db.Column(db.Integer, primary_key=True)
+#     PostingUserID = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     UserPhoneNumer = db.Column(db.String(20), nullable=False)
+#     MoneyAmount = db.Column(db.Float, nullable=False)
+#     Usd_To_Lbp = db.Column(db.Boolean, nullable=False)
+#     Resolved = db.Column(db.Boolean, nullable=True)
+#     ResolvedByUser = db.Column(db.Integer, nullable=True)
+
+#     # def __init__(self, PostingUserID, UserPhoneNumer, MoneyAmount, Usd_To_Lbp, Resolved, ResolvedByUser):
+#     #     super(Listing, self).__init__()
+
+# class ListingSchema(ma.Schema):
+#     class Meta:
+#         fields = ('ListingID', 'PostingUserID', 'UserPhoneNumer', 'MoneyAmount', 'Usd_To_Lbp', 'Resolved', 'ResolvedByUser')
+#         model = Listing
+
